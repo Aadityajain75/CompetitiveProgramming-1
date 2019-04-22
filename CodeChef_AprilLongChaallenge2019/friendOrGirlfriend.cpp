a305d1acaee189be5f37ed3ad6477f79e0dfefc0
iamarchit2 <architaggarwal0508@gmail.com>
2019-04-22 14:14:13 +0530

    Modified

---
+++ CodeChef_AprilLongChaallenge2019/friendOrGirlfriend.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main() {
+	ll int T;
+	cin >> T;
+	while(T--) {
+		ll int N;
+		cin >> N;
+		string A;
+		cin >> A;
+		char a;
+		cin >> a;
+		ll int findex = -1;
+		ll ans = 0;
+		for(ll int i = 0; i < N; i++) {
+			if(A[i] == a) {
+				ans += i + 1;
+				ans += (findex + 1) * ((i - 1) - findex);
+				findex = i;
+			}
+		}
+		if(findex != N - 1) {
+			ans+= (findex + 1) * ((N - 1) - findex);
+		}
+		cout << ans << endl;
+	}
+}
