f72a88e8c39e33bb1c7ee02fc3460b9ea1808385
iamarchit2 <architaggarwal0508@gmail.com>
2019-04-24 10:58:44 +0530

    Modified

---
+++ CodeChef_NovemberLongChallenge2018/chefAndDifficultContests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    long long int T;
+    cin >> T;
+    while (T--) {
+        long long int a, b;
+        cin >> a >> b;
+        cout << "YES" << endl;
+    }
+}
